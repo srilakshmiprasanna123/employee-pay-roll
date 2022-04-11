32add3e86d87f4eb443f8ce8d116030833971705
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-12 04:45:41 +0530

    welcome message

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to employee pay role");
+    }
+}

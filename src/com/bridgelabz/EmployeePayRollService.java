3e2e4b70a8a89ff7fc8fa98638c80b0c1c3b411c
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-12 06:24:38 +0530

    added file operation

--- src/com/bridgelabz/EmployeePayRollService.java
+++ src/com/bridgelabz/EmployeePayRollService.java
@@ -1,2 +1,38 @@
-package com.bridgelabz;public class EmployeePayRollService {
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayRollService {
+    private List<EmployeePayrollData> employeePayrollList;
+    public EmployeePayRollService() {
+    }
+    public EmployeePayRollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+        EmployeePayRollService employeePayrollService = new EmployeePayRollService(employeePayrollList);
+        employeePayrollService.readEmployeeData(scanner);
+        employeePayrollService.write();
+
+    }
+
+    private void readEmployeeData(Scanner scanner) {
+        System.out.println("Enter your name : ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your ID : ");
+        int id = scanner.nextInt();
+        System.out.println("Enter your salary : ");
+        double salary = scanner.nextDouble();
+        EmployeePayrollData empData = new EmployeePayrollData(id, salary, name);
+        employeePayrollList.add(empData);
+    }
+    private void write() {
+        System.out.println("Given Employee Data is : " + employeePayrollList);
+    }
 }
+
--- src/com/bridgelabz/EmployeePayrollData.java
+++ src/com/bridgelabz/EmployeePayrollData.java
@@ -1,2 +1,49 @@
-package com.bridgelabz;public class EmployeePayrollData {
+package com.bridgelabz;
+
+public class EmployeePayrollData {
+    private int id;
+    private double salary;
+    private String name;
+
+    public EmployeePayrollData(int id, double salary, String name) {
+        this.id = id;
+        this.salary = salary;
+        this.name = name;
+    }
+    public EmployeePayrollData(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollData{" +
+                "id=" + id +
+                ", salary=" + salary +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
